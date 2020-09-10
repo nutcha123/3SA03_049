6268fe30b10d838a9a966bfe8323354856f8fe1a
nutcha123 <nutcha.ptc@gmail.com>
2020-09-10 10:46:10 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
-  return (
-      <div>
-      Hello World
-      </div>
-  );
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
 }
+
 
 export default App;
